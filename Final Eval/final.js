ab17deba58254511ffbdd02d2ce3d1320f69c983
Karituo-Luster <55764170+Karituo-Luster@users.noreply.github.com>
2022-08-27 02:11:01 -0500

    Add files via upload

--- Final Eval/final.js
+++ Final Eval/final.js
@@ -1,9 +1,48 @@
-import {Api} from './api.js'
-/*NOTE:
-     I have errands that I cannot ignore this Friday, so I will be posting what I have produced
-     in the span of two hours. I will be working on it by the time I get back home.
-     - TOMMY HOANG
-*/
+/* API */
+const Api = (() =>{
+     const baseURL = 'http://localhost:4232';
+     const path = 'courseList';
+
+     const getCourses = fetch([baseURL, path].join('/')).then((response) => response.json());
+
+     return {
+          getCourses,
+     };
+})();
+/**********************************************************/
+/* View */
+const View = (() =>{
+     //All objects required for this project
+     const blocks = {
+          courseList: "coursesList",
+          leftContainer: '#leftContainer',
+          rightContainer: '#rightContainer',
+          courses: '.courses',
+     };
+     const render = (ele, tmp) => {
+          ele.innerHTML = tmp;
+     };
+     //Set all courses in the left container
+     const createTmp = (arr) => {
+          let tmp, required = '';
+          arr.forEach((course) => {
+               if(course.required === true){
+                    required = 'Compulsory';
+               }
+               else{
+                    required = 'Elective';
+               }
+               tmp+= '<li> <span>${course.courseName} - ${course.required} - Credits: ${course.credit}</span> </li>';
+          });
+          return tmp;
+     }
+
+     return {
+          blocks,
+          render,
+          createTmp,
+     };
+})();
 
 /* Model */
 const Model = ((api, view) =>{
@@ -14,12 +53,12 @@ const Model = ((api, view) =>{
                this.courseName = courseName;
                this.required = required;
                this.credit = credit;
-               this.flag = false;
           }
      }
      class State{
           //Create array for list of courses
           #courses = [];
+          #selectedCourses = [];
 
           get courseList(){
                return this.#courses;
@@ -36,39 +75,10 @@ const Model = ((api, view) =>{
 
      return{
           getCourses,
-          selectCourses,
           Course,
           State,
      };
 })(Api, View);
-
-/* View */
-const View = (() =>{
-     //All objects required for this project
-     const blocks = {
-          leftContainer: '#leftContainer',
-          rightContainer: '#rightContainer',
-          courses: '.courses',
-          selectButton: '.selectButton',
-     };
-     const render = (ele, tmp) => {
-          ele.innerHTML = tmp;
-     };
-     //Show all courses in the left container
-     const createTmp = (arr) => {
-          let tmp = '';
-          arr.forEach(availableCourses => {
-               tmp+='<li> <span>${Course.courseID} - ${Course.courseName} - Credits: ${Course.credit}</span> </li>';
-          });
-          return tmp;
-     }
-
-     return {
-          blocks,
-          render,
-          createTmp,
-     };
-})();
 
 /* Controller 
 When user select course, the credit of that course should be add up to the total credit counter
@@ -83,43 +93,26 @@ If user click "OK" button in the pop up window, all the selected courses will be
 If user click "Cancel", window will be closed and you still have all the functionalities from the previous features
 After User successfully selected all the courses, "Select" button should be disabled
 */
-
-
 const Controller = ((model, view) =>{
      const state = new model.State();
      let creditHours = 0;
-     const selectingCourse = () =>{
-          view.blocks.courses.addEventListener('click', function onClick()){
-               //change the specific course flag to true
-               model.Course.flag = true;
-               //Add total credit house
-               creditHours+= model.Course.credit;
-               
-          }
-          //clicking course change color and adds flag to selected
-          
-     }
-     const deselectCourse = () =>{
-          //clicking on a selected course decolorizes the course and subtracts credit hours.
-          view.blocks.courses.addEventListener('click', function onClick){
-               //change specific course flag to false
-               model.Course.flag = false;
-               //Subtract total credit hours
-               creditHours -= model.Course.credit;
-          }
-     }
      if(creditHours > 18){
           alert("You can only choose up to 18 credits in one semester.");
      }
-     view.blocks.selectButton.addEventListener('click', function onClick()){
-          if(confirm("You have selected up to ${creditHours}. You cannot change once you click \"OK\".") === true){
-               //put selected courses into right container
-               
-               //lock select button
-               view.blocks.selectButton.disable() = true;
-          }
+     
+
+     const init = () => {
+          model.getCourses().then((course) => {
+               state.courseList = [...course.reverse()];
+          });
+     };
+     const bootstrap = () => {
+          init();
      }
 
+     return{
+          bootstrap,
+     };
 })(Model, View);
 
 Controller.bootstrap();
